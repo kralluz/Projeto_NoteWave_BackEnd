9d15d02d50cf4bda137c2b959d537b64c3d2498f
Carlos Henrique <97392902+kralluz@users.noreply.github.com>
2023-11-09 17:58:22 -0300

    cria  a tabela

--- src/logic.ts
+++ src/logic.ts
@@ -2,7 +2,16 @@ import { Request, Response } from "express";
 import {  client } from "./database";
 
 export const readProducts = async (req: Request, res: Response) => {
-    const queryString = ` SELECT * FROM notion_clone; `;
+    const queryString = `
+    CREATE TABLE notes (
+        id SERIAL PRIMARY KEY,
+        tittle VARCHAR(255) NOT NULL,
+        conteudo TEXT,
+        page_id INT,
+        data_criacao TIMESTAMP NOT NULL,
+        data_atualizacao TIMESTAMP NOT NULL,
+    );
+     SELECT * FROM notion_clone; `;
     const movie = await client.query(queryString);
     return res.status(200).json(movie.rows[1]);
 };
