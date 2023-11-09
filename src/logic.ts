0e13478a329d70f3784401d51c8dbda8f1a46231
Carlos Henrique <97392902+kralluz@users.noreply.github.com>
2023-11-09 17:53:47 -0300

    fix name table

--- src/logic.ts
+++ src/logic.ts
@@ -2,7 +2,7 @@ import { Request, Response } from "express";
 import {  client } from "./database";
 
 export const readProducts = async (req: Request, res: Response) => {
-    const queryString = ` SELECT * FROM notes; `;
+    const queryString = ` SELECT * FROM notion_clone; `;
     const movie = await client.query(queryString);
     return res.status(200).json(movie.rows[1]);
 };
