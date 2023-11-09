43375e73f1c35dbe232a127e5f65b58c2837759e
Carlos Henrique <97392902+kralluz@users.noreply.github.com>
2023-11-09 17:03:05 -0300

    reset

--- src/interfaces.ts
+++
@@ -1,9 +0,0 @@
-export interface Product {
-    id: number;
-    name: string;
-    price: number;
-    weight: number;
-    section: "food" | "cleaning";
-    calories?: number | null | undefined;
-    expirationDate: Date;
-}
--- src/logic.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response } from "express";
-import {  client } from "./database";
-
-export const readProducts = async (req: Request, res: Response) => {
-    const queryString = ` SELECT * FROM notes; `;
-    const movie = await client.query(queryString);
-    return res.status(200).json(movie.rows[1]);
-};
