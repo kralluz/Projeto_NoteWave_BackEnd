dcd4c6a2c880d5a95e4d3b9be3e3befc3e4f0b00
Carlos Henrique <97392902+kralluz@users.noreply.github.com>
2023-11-09 17:00:33 -0300

    tinha dado errado, agora da certo

---
+++ src/logic.ts
@@ -0,0 +1,8 @@
+import { Request, Response } from "express";
+import {  client } from "./database";
+
+export const readProducts = async (req: Request, res: Response) => {
+    const queryString = ` SELECT * FROM notes; `;
+    const movie = await client.query(queryString);
+    return res.status(200).json(movie.rows[1]);
+};
